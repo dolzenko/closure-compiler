0a4eef5d0e3b59c534b2581ca388f45e81547cba
Richard Cao <richard@mtggoldfish.com>
2021-05-21 11:36:26 -0700

    Remove incorrect flag.

--- lib/closure/compiler.rb
+++ lib/closure/compiler.rb
@@ -15,7 +15,6 @@ module Closure
     DEFAULT_OPTIONS = {
       :warning_level => 'QUIET',
       :language_in => 'ECMASCRIPT6',
-      :language => 'ECMASCRIPT6'
     }
 
     # When you create a Compiler, pass in the flags and options.
