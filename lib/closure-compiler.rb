e668bcc833b6eab5f9d20748784eafc819df8bc8
Richard Cao <richard@mtggoldfish.com>
2021-05-21 10:51:39 -0700

    Update.

--- lib/closure-compiler.rb
+++ lib/closure-compiler.rb
@@ -1,8 +1,8 @@
 module Closure
 
-  VERSION           = "1.1.14"
+  VERSION           = "1.1.15"
 
-  COMPILER_VERSION  = "20180506"
+  COMPILER_VERSION  = "v20210505"
 
   JAVA_COMMAND      = 'java'
 
